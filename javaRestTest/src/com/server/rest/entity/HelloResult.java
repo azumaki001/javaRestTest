01adb0fdff128fa7e0f872744b4714ed005e8778
azumaki001 <38251836+azumaki001@users.noreply.github.com>
2018-04-18 12:55:55 +0900

    Add files via upload

---
+++ javaRestTest/src/com/server/rest/entity/HelloResult.java
@@ -0,0 +1,19 @@
+package com.server.rest.entity;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class HelloResult {
+
+	public String resultCode;
+	public String errorMessage;
+	public String processingId;
+
+	public String mebr_no ="";
+	public String cust_id ="";
+	public String ufpr_mebr_no;
+	public String able_i_nam;
+	public String flg;
+	public String nb;
+
+}
